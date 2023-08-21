9032a5fe885a7f3919bba38ee938fd563e133872
Afloresep <afloresep01@gmail.com>
2023-08-21 12:03:45 +0200

    added split folder to new train, test, validation folders function

--- src/tools.py
+++ src/tools.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from shutil import move
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import load_img, img_to_array
-
+import argparse
 import shutil
 from sklearn.model_selection import train_test_split
 
@@ -28,50 +28,73 @@ def plot_sample_images(directory, num_samples=5):
 
 
 def split_folder_to_train_test_valid(data_directory):
-    # Define paths to the original data and the new train/test/validation directories
-    original_data_dir = data_directory
-    train_dir = original_data_dir+'/train'
-    test_dir = original_data_dir+'/test'
-    validation_dir = original_data_dir+'/validation'
+    """
+    Splits data from the original directory into train, test, and validation directories for each class.
+    
+    Args:
+        data_directory (str): Path to the original data directory containing subdirectories for each class.
+    """
 
-    # Create the new train/test/validation directories if they don't exist
-    os.makedirs(train_dir, exist_ok=True)
-    os.makedirs(test_dir, exist_ok=True)
-    os.makedirs(validation_dir, exist_ok=True)
+    original_data_dir = '../data/cell_images'
+    train_dir = '../data/cell_images/train'
+    test_dir = '../data/cell_images/test'
+    validation_dir = '../data/cell_images/validation'
 
     # Get a list of class subdirectories in the original data directory
     class_subdirectories = [d for d in os.listdir(original_data_dir) if os.path.isdir(os.path.join(original_data_dir, d))]
 
-    # Split the data for each class into train, test, and validation sets
-    for class_subdir in class_subdirectories:
-        class_path = os.path.join(original_data_dir, class_subdir)
-        class_images = [img for img in os.listdir(class_path) if img.endswith('.png')]
-        
-        # Split the images for the current class into train, test, and validation sets (70% 15% 15%)
+    for class_subdir in class_subdirectories: # for 'Class' in ['Class1', 'Class2']
+        class_path = os.path.join(original_data_dir, class_subdir) # ../data/cell_images/Parasitized and ../data/cell_images/Uninfected
+        class_images = [img for img in os.listdir(class_path)] # ['C13NThinF_IMG_20150614_131318_cell_179.png'... all images for each class
+
         train_images, test_validation_images = train_test_split(class_images, test_size=0.3, random_state=42)
         test_images, validation_images = train_test_split(test_validation_images, test_size=0.5, random_state=42)
-        
-        # Create class subdirectories in train, test, and validation directories
+
+        print(f'Total images for class {class_subdir}: ', len(class_images)) # 13780 (all images for class_subdir) or 100%
+        print(f'Total train images: {len(train_images)} or {(len(train_images)/len(class_images))*100}% ') # 9646 (70% of all images from class_images)
+        print(f'Total test images: {len(test_images)} or {len(test_images)/len(class_images)*100}% ') # 2067 class 1 15%
+        print(f'Total validation images: {len(validation_images)} or {len(validation_images)/len(class_images)*100}% ')
+        print('\n')
+
+
+        # Create Class 1 and Class 2 subdirectories inside new folders
         train_class_dir = os.path.join(train_dir, class_subdir)
-        test_class_dir = os.path.join(test_dir, class_subdir)
-        validation_class_dir = os.path.join(validation_dir, class_subdir)
-        
         os.makedirs(train_class_dir, exist_ok=True)
+
+        test_class_dir = os.path.join(test_dir, class_subdir)
         os.makedirs(test_class_dir, exist_ok=True)
+
+        validation_class_dir = os.path.join(validation_dir, class_subdir)
         os.makedirs(validation_class_dir, exist_ok=True)
-        
-        # Move images to the appropriate directories
+
+        # Move images from each class folder to new subdirectories
         for img in train_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(train_class_dir, img)
+            src_path =os.path.join(class_path, img)
+            dst_path =os.path.join(train_class_dir,img)
             shutil.copy(src_path, dst_path)
-            
+
+
         for img in test_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(test_class_dir, img)
+            src_path =os.path.join(class_path, img)
+            dst_path =os.path.join(test_class_dir,img)
             shutil.copy(src_path, dst_path)
-            
+
+
         for img in validation_images:
-            src_path = os.path.join(class_path, img)
-            dst_path = os.path.join(validation_class_dir, img)
+            src_path =os.path.join(class_path, img)
+            dst_path =os.path.join(validation_class_dir,img)
             shutil.copy(src_path, dst_path)
+
+    folder_dir = []
+    for x in ['train', 'test', 'validation']: folder_dir.append((os.path.join('../data/cell_images/', x)))
+
+    for i in folder_dir:
+        for name in os.listdir(i):
+            print(i, name ,len(os.listdir(os.path.join(i,name))))
+        # Print new sizes for each new folder
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Split data from original directory into train, test, and validation directories.")
+        parser.add_argument("data_directory", type=str, help="Path to the original data directory")
+        args = parser.parse_args()
+        
+        split_folder_to_train_test_valid(args.data_directory)
